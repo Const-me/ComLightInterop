f8b9b19d58113ecf7e96175a846f81fb100cc719
Konstantin Gontarenko <const@const.me>
2019-08-31 21:27:22 +0200

    Minor, assembly info

---
+++ ComLight/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle( "ComLight" )]
+[assembly: AssemblyDescription( "Lightweight cross-platform COM interop library for Windows and Linux. Allows to expose C++ objects to .NET, and .NET objects to C++." )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "ComLight" )]
+[assembly: AssemblyCopyright( "Copyright © const.me, 2019" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+[assembly: ComVisible( false )]
+
+[assembly: Guid( "16d6c3fb-2a8f-4134-a4b3-819411f2c595" )]
+
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
