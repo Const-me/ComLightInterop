c5f233d2637e0e4152990feb4f66b9404fd44a5d
Konstantin <const@const.me>
2020-02-29 00:05:01 +0100

    Comments

--- ComLight/ComInterfaceAttribute.cs
+++ ComLight/ComInterfaceAttribute.cs
@@ -17,9 +17,11 @@ namespace ComLight
 	[AttributeUsage( AttributeTargets.Interface, Inherited = false )]
 	public class ComInterfaceAttribute: Attribute
 	{
-		/// <summary>COM interface ID for this interface, must match with the value in DEFINE_INTERFACE_ID macro in C++ code.</summary>
+		/// <summary>COM interface ID for this interface, must match the value in DEFINE_INTERFACE_ID macro in C++ code.</summary>
 		public readonly Guid iid;
+
 		/// <summary>You can limit the direction of the marshaling, e.g. it makes no sense to implement something like ID3D11Buffer in C#, it won't work.</summary>
+		/// <remarks>Single-direction marshaling is more efficient than the default <see cref="eMarshalDirection.BothWays" />.</remarks>
 		public readonly eMarshalDirection marshalDirection;
 
 		/// <summary>Construct by parsing a string GUID</summary>
