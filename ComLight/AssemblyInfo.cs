46c7fb9f21695586af9e1512cca87b24335ed91a
Konstantin <const@const.me>
2020-03-30 22:25:56 +0200

    Version 1.3.6

--- ComLight/AssemblyInfo.cs
+++ ComLight/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid( "16d6c3fb-2a8f-4134-a4b3-819411f2c595" )]
 
-[assembly: AssemblyVersion( "1.3.5.0" )]
-[assembly: AssemblyFileVersion( "1.3.5.0" )]
+[assembly: AssemblyVersion( "1.3.6.0" )]
+[assembly: AssemblyFileVersion( "1.3.6.0" )]
