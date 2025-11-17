b107bf2959e819f0e67f0c1916131376061be9bc
Const-me <const@const.me>
2025-11-17 17:26:18 +0100

    Bugfix, random order of COM methods for partial interfaces

--- ComLightGenerator/Emit/IfaceMeta.cs
+++ ComLightGenerator/Emit/IfaceMeta.cs
@@ -20,6 +20,15 @@ readonly struct IfaceMeta
 	public readonly bool classFactory;
 	public string name => iface.Name;
 
+	static string sourceFile( ComMethod cm )
+	{
+		SyntaxReference syntaxRef = cm.method.DeclaringSyntaxReferences.First();
+		SyntaxNode syntaxNode = syntaxRef?.GetSyntax( default )!;
+		SyntaxTree tree = syntaxNode?.SyntaxTree!;
+		string path = tree?.FilePath!;
+		return Path.GetFileNameWithoutExtension( path );
+	}
+
 	public IfaceMeta( in ComInterface iface, in MarshallerMethods marshaller )
 	{
 		this.iface = iface.iface;
@@ -32,7 +41,20 @@ readonly struct IfaceMeta
 		for( int i = 0; i < list.Count; i++ )
 			methods[ i ] = new ComMethod( list[ i ], iface );
 
+		if( iface.iface.isPartial() )
+		{
+			var withNames = methods.Select( m => (m, sourceFile( m )) ).ToList();
+			methods = withNames
+				.OrderBy( t => t.Item2, StringComparer.OrdinalIgnoreCase )
+				.Select( t => t.Item1 )
+				.ToArray();
+		}
+
+		Array.Sort( methods, compareNames );
 		isPartial = iface.iface.isPartial();
 		classFactory = iface.iface.hasAttribute( AttributeNames.classFactory );
+
+		static int compareNames( ComMethod a, ComMethod b ) =>
+			string.Compare( a.name, b.name, StringComparison.Ordinal );
 	}
 }
